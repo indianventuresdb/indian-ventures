236eee790d416ea35c873d90cdd99d73c13809e0
Priya Tiwari <87354985+08pt@users.noreply.github.com>
2024-04-10 12:48:00 +0530

    add color background

--- src/components/Navbar/Navlinks.js
+++ src/components/Navbar/Navlinks.js
@@ -53,38 +53,38 @@ const Navlinks = () => {
         <BsList className="h-8 w-8 text-mblack" />
       </div>
       {isMenuOpen && (
-        <div className="md:hidden absolute top-0 left-0 w-[60%] sm:w-1/3 h-80 bg-black z-50 flex flex-col px-2 pt-10 py-4">
+        <div className="md:hidden absolute top-16 left-0 w-[60%] sm:w-1/3 h-80 bg-white z-50 flex flex-col px-2 pt-10 py-4">
           <Link
             href="/"
-            className="text-white
+            className="text-mblack
              text-lg font-semibold mb-4 hover:text-gray-500"
           >
             Home
           </Link>
           <Link
             href="/about"
-            className="text-white
+            className="text-mblack
              text-lg font-semibold mb-4 hover:text-gray-500"
           >
             About us
           </Link>
           <Link
             href="/technologies"
-            className="text-white
+            className="text-mblack
              text-lg font-semibold mb-4 hover:text-gray-500"
           >
             Technologies
           </Link>
           <Link
             href="/services"
-            className="text-white
+            className="text-mblack
              text-lg font-semibold mb-4 hover:text-gray-500"
           >
             Services
           </Link>
           <Link
             href="/contact"
-            className="text-white
+            className="text-mblack
              text-lg font-semibold hover:text-gray-500"
           >
             Contact us
