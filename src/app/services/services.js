f3db514e4aaef2753d4f2679b26e7a626b108b45
Priya Tiwari <87354985+08pt@users.noreply.github.com>
2024-04-18 23:09:00 +0530

    fixed layout

--- src/app/services/services.js
+++ src/app/services/services.js
@@ -55,7 +55,7 @@ const Services = () => {
           key={index}
           className="grid grid-cols-1 md:grid-cols-2 border-4 border-[#D9D9D9] shadow-md sm:rounded-3xl bg-[#ECECEC] "
         >
-          <div className=" w-[80%] relative  min-h-[16rem] sm:rounded-t-3xl sm:rounded-bl-3xl ">
+          <div className=" w-full lg:w-[80%] relative  min-h-[16rem] sm:rounded-t-3xl sm:rounded-bl-3xl ">
             <Image
               src={service.image}
               alt={`Service ${index + 1}`}
@@ -64,11 +64,13 @@ const Services = () => {
               className=" md:rounded-l-3xl sm:rounded-none"
             />
           </div>
-          <div className="w-[80%] flex flex-col justify-center">
+          <div className="w-full lg:w-[80%] flex flex-col justify-center">
             <h2 className="text-xl font-bold mb-4 text-center">
               {service.heading}
             </h2>
-            <p className="text-justify ">{service.content}</p>
+            <p className="text-justify  justify-center px-4  mx-auto">
+              {service.content}
+            </p>
           </div>
         </div>
       ))}
