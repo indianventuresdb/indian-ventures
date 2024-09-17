8a823e8d747e671d5a9dc74b6542d5bf53400096
Priya Tiwari <87354985+08pt@users.noreply.github.com>
2024-09-17 15:30:32 +0530

    done

--- src/app/layout.js
+++ src/app/layout.js
@@ -27,17 +27,15 @@ export default function RootLayout({ children }) {
       <body className={`${manrope_init.variable}`}>
         <Navbar />
         <div className="flex justify-center items-center mt-4">
-          <Link href="/">
-            <a className="flex items-center">
-              <Image
-                src="/favicon.ico"
-                alt="Site Logo"
-                width={48}
-                height={48}
-                className="mr-2"
-              />
-              <h1 className="text-3xl font-bold">{metadata.title}</h1>
-            </a>
+          <Link href="/" className="flex items-center">
+            <Image
+              src="/favicon.ico"
+              alt="Site Logo"
+              width={48}
+              height={48}
+              className="mr-2"
+            />
+            <h1 className="text-3xl font-bold">{metadata.title}</h1>
           </Link>
         </div>
         <div className="max-w-screen-2xl mx-auto">{children}</div>
